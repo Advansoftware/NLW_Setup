a7c99178f059fe6810d4951d3cbcde7f400cd3e8
Advansoftware <brunoantunes94@hotmail.com>
2023-01-19 16:35:05 -0300

    add videoplay

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import React from 'react';
+import "./video-react.css";
+import { Player, BigPlayButton } from 'video-react';
+import {Container, Box} from '@mui/material';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      <Container>
+        <Box>
+          <Player src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
+            <BigPlayButton position="center" />
+          </Player>
+        </Box>
+    </Container>
   )
 }
 
-export default App
+export default App;
